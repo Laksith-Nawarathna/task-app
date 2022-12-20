85abd0f2e3f857547f0d58a4315ade76f1fb473b
Eranga Bandara <erangabandara94@gmail.com>
2022-12-20 18:33:09 +0530

    Add validation for create user account

--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -3,14 +3,24 @@ package lk.ijse.dep9.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
 
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z ]+$")
     private String fullName;
+    @NotBlank
     private String username;
+    @NotEmpty
+    @Length(min = 3)
     private String password;
 
 }
