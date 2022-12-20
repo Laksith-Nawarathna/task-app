3dc52990f3e478a242dc02fa9d2826cecc7c51b2
Eranga Bandara <erangabandara94@gmail.com>
2022-12-20 13:36:48 +0530

    Add entities

---
+++ src/main/java/lk/ijse/dep9/app/entity/Task.java
@@ -0,0 +1,35 @@
+package lk.ijse.dep9.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task implements SuperEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String content;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @JoinColumn(name = "project_id", referencedColumnName = "id", nullable = false)
+    @ManyToOne
+    private Project project;
+
+    public Task(String content, Status status, Project project) {
+        this.content = content;
+        this.status = status;
+        this.project = project;
+    }
+
+    public static enum Status{
+        COMPLETED, NOT_COMPLETED
+    }
+}
