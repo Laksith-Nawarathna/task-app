ee583994255f5c9a72cd097d90d57180e3b95f5f
Eranga Bandara <erangabandara94@gmail.com>
2022-12-21 12:15:00 +0530

    Finalize exposing web services related to the Users

--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -1,9 +1,11 @@
 package lk.ijse.dep9.app.api;
 
 import lk.ijse.dep9.app.dto.UserDTO;
+import lk.ijse.dep9.app.util.ValidationGroups;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -34,18 +36,20 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/me", consumes = "application/json")
-    public void updateUserAccountDetails(@Valid @RequestBody UserDTO user) {
+    public void updateUserAccountDetails(@Validated(ValidationGroups.Create.class) @RequestBody UserDTO user) {
         System.out.println(user);
     }
 
-    @GetMapping("/me")
-    public void getUserAccountDetails() {
-
+    @GetMapping(value = "/me", produces = "application/json")
+    public UserDTO getUserAccountDetails() {
+        System.out.println("getUserAccountDetails()");
+        return new UserDTO();
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/me")
     public void deleteUserAccount() {
-
+        System.out.println("deleteUserAccount()");
     }
 
 }
--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep9.app.dto;
 
+import lk.ijse.dep9.app.util.ValidationGroups;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +19,7 @@ public class UserDTO implements Serializable {
     @NotBlank(message = "Full Name cannot be empty or null")
     @Pattern(regexp = "^[A-Za-z ]+$", message = "Invalid name")
     private String fullName;
-    @NotBlank(message = "Username cannot be empty or null")
+    @NotBlank(message = "Username cannot be empty or null", groups = ValidationGroups.Create.class)
     private String username;
     @NotEmpty(message = "Password cannot be empty or null")
     @Length(min = 3, message = "Password should contain at least 3 characters")
