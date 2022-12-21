d28220af0faa36f1561a027a45aba50873146ef8
Eranga Bandara <erangabandara94@gmail.com>
2022-12-21 11:50:11 +0530

    Handle validation exceptions smoothly

--- src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
+++ src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
@@ -1,27 +1,35 @@
 package lk.ijse.dep9.app.advice;
 
-import lk.ijse.dep9.app.dto.ErrorResponseMessageDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Throwable.class)
-    public ErrorResponseMessageDTO uncaughtExceptions(Throwable t){
-        t.printStackTrace();
-        return new ErrorResponseMessageDTO(t.getMessage(), 405);
-    }
+//    @ExceptionHandler(Throwable.class)
+//    public ErrorResponseMessageDTO uncaughtExceptions(Throwable t){
+//        t.printStackTrace();
+//        return new ErrorResponseMessageDTO(t.getMessage(), 405);
+//    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponseMessageDTO validationExceptions(MethodArgumentNotValidException e){
-        String message = e.getFieldErrors().stream().map(err -> err.getField() + ": " + err.getDefaultMessage() + ", ")
-                .reduce((prev, curr) -> prev + curr).get();
-        return new ErrorResponseMessageDTO(message, 400);
+    public Map<String, Object> validationExceptions(MethodArgumentNotValidException e){
+        HashMap<String, Object> errorAttributes = new LinkedHashMap<>();
+        errorAttributes.put("status", HttpStatus.BAD_REQUEST.value());
+        errorAttributes.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        errorAttributes.put("timestamp", new Date().toString());
+        List<String> validationErrList = e.getFieldErrors().stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .collect(Collectors.toList());
+        errorAttributes.put("errors", validationErrList);
+        return errorAttributes;
     }
 
 }
